5314df086e05a668c3cde26c0fe5dff7be894d65
ashwin <ashwinath@hotmail.com>
2017-04-17 21:52:30 +0800

    refactored method to accomadate different type of message types.

--- src/main/java/com/ashwinchat/starsimulator/impl/MessageHandlerImpl.java
+++ src/main/java/com/ashwinchat/starsimulator/impl/MessageHandlerImpl.java
@@ -10,7 +10,7 @@ public class MessageHandlerImpl {
     private StarSimulatorImpl simulator;
     private static MessageHandlerImpl instance = new MessageHandlerImpl();
     private static final Logger logger = Logger.getLogger(StarSimulatorImpl.class);
-    private static final String COMMAND = "/star";
+    private static final String MAPLE_COMMAND = "/star";
     private static final int NORMAL_MAX_STARS = 20;
     private static final int SUPERIOR_MAX_STARS = 15;
     private static final int REASONABLE_SUPERIOR_MAX_STARS = 13;
@@ -24,45 +24,51 @@ public class MessageHandlerImpl {
     }
 
     public String processAndFormatReply(String[] userInput, String content) {
+        if (userInput.length > 0 && StringUtils.equals(userInput[0], MAPLE_COMMAND)) {
+            String messageToSend = handleMapleResponse(userInput, content);
+            return messageToSend;
+        }
+        return null;
+    }
+
+    private String handleMapleResponse(String[] userInput, String content) {
         String messageToSend = null;
-        if (userInput.length > 0 && StringUtils.equals(userInput[0], COMMAND)) {
-            logger.info("Message Received: " + content);
-            if (userInput.length != 3) {
-                messageToSend = "USAGE: /star <normal/superior> <desired stars>";
-                logger.warn("Wrong number of argument given. # of arguments given = " + userInput.length);
-            } else {
-                String userItemTypeInput = StringUtils.upperCase(userInput[1]);
-                try {
-                    int desiredStarLevel = Integer.parseInt(userInput[2]);
-                    if (StringUtils.equals(userItemTypeInput, StringUtils.upperCase(ItemType.NORMAL.getItemDescription()))) {
-                        if (desiredStarLevel <= NORMAL_MAX_STARS) {
-                            StarResult result = this.simulator.runSimulation(desiredStarLevel, ItemType.NORMAL);
+        logger.info("Message Received: " + content);
+        if (userInput.length != 3) {
+            messageToSend = "USAGE: /star <normal/superior> <desired stars>";
+            logger.warn("Wrong number of argument given. # of arguments given = " + userInput.length);
+        } else {
+            String userItemTypeInput = StringUtils.upperCase(userInput[1]);
+            try {
+                int desiredStarLevel = Integer.parseInt(userInput[2]);
+                if (StringUtils.equals(userItemTypeInput, StringUtils.upperCase(ItemType.NORMAL.getItemDescription()))) {
+                    if (desiredStarLevel <= NORMAL_MAX_STARS) {
+                        StarResult result = this.simulator.runSimulation(desiredStarLevel, ItemType.NORMAL);
+                        messageToSend = StarUtils.formatStarString(result);
+                    } else {
+                        messageToSend = "Normal items can only be starred to " + NORMAL_MAX_STARS + " stars.";
+                        logger.warn("Person sent number greater than " + NORMAL_MAX_STARS + ". Number = " + desiredStarLevel);
+                    }
+                } else if (StringUtils.equals(userItemTypeInput, StringUtils.upperCase(ItemType.SUPERIOR.getItemDescription()))) {
+                    if (desiredStarLevel <= SUPERIOR_MAX_STARS) {
+                        if (desiredStarLevel <= REASONABLE_SUPERIOR_MAX_STARS) {
+                            StarResult result = this.simulator.runSimulation(desiredStarLevel, ItemType.SUPERIOR);
                             messageToSend = StarUtils.formatStarString(result);
                         } else {
-                            messageToSend = "Normal items can only be starred to " + NORMAL_MAX_STARS + " stars.";
-                            logger.warn("Person sent number greater than " + NORMAL_MAX_STARS + ". Number = " + desiredStarLevel);
-                        }
-                    } else if (StringUtils.equals(userItemTypeInput, StringUtils.upperCase(ItemType.SUPERIOR.getItemDescription()))) {
-                        if (desiredStarLevel <= SUPERIOR_MAX_STARS) {
-                            if (desiredStarLevel <= REASONABLE_SUPERIOR_MAX_STARS) {
-                                StarResult result = this.simulator.runSimulation(desiredStarLevel, ItemType.SUPERIOR);
-                                messageToSend = StarUtils.formatStarString(result);
-                            } else {
-                                messageToSend = "Simulations for superior items over " + REASONABLE_SUPERIOR_MAX_STARS + " stars take too long.";
-                                logger.warn("Person tried for starring simulation greater than " + REASONABLE_SUPERIOR_MAX_STARS + " stars.");
-                            }
-                        } else {
-                            messageToSend = "Superior items can only be starred to " + SUPERIOR_MAX_STARS + " stars.";
-                            logger.warn("Person sent number greater than " + SUPERIOR_MAX_STARS + ". Number = " + desiredStarLevel);
+                            messageToSend = "Simulations for superior items over " + REASONABLE_SUPERIOR_MAX_STARS + " stars take too long.";
+                            logger.warn("Person tried for starring simulation greater than " + REASONABLE_SUPERIOR_MAX_STARS + " stars.");
                         }
                     } else {
-                        messageToSend = "I can only simulate normal or superior items.";
-                        logger.warn("Person named an unknown item the 2nd field. Item = " + userItemTypeInput);
+                        messageToSend = "Superior items can only be starred to " + SUPERIOR_MAX_STARS + " stars.";
+                        logger.warn("Person sent number greater than " + SUPERIOR_MAX_STARS + ". Number = " + desiredStarLevel);
                     }
-                } catch (NumberFormatException e) {
-                    messageToSend = "Number of stars must be an integer.";
-                    logger.warn("Person did not send an integer in the 3rd field.");
+                } else {
+                    messageToSend = "I can only simulate normal or superior items.";
+                    logger.warn("Person named an unknown item the 2nd field. Item = " + userItemTypeInput);
                 }
+            } catch (NumberFormatException e) {
+                messageToSend = "Number of stars must be an integer.";
+                logger.warn("Person did not send an integer in the 3rd field.");
             }
         }
         return messageToSend;
